76cf77fd94557adc6f8fff6e99cf054a71334db0
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-08 22:40:15 +0530

    [Ankit]Add : Prime Factors of number and Second Smallest and Largest in Array

---
+++ Day7Assignments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day7Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 7 Assignment");
+            PrimeFactorization obj1 = new PrimeFactorization();
+            obj1.PrimeFactors();
+            SecondLargestAndSmallest obj2 = new SecondLargestAndSmallest();
+            obj2.Second();
+        }
+    }
+}
---
+++ Day7Assignments/SecondLargestAndSmallest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Day7Assignments
+{
+    public class SecondLargestAndSmallest
+    {
+        public void Second()
+        {
+            int size, largest, positionOfLargest = 0 , positionOfSmallest = 0, secondLargest, smallest, secondSmallest, i;
+            Console.WriteLine("Input the size of array : ");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("Enter Elements in the Array");
+            for (i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            largest = array[0];
+            for (i = 0; i < size; i++)
+            {
+                if (array[i] > largest)
+                {
+                    largest = array[i];
+                    positionOfLargest = i;
+                }
+            }
+            smallest = array[0];
+            for (i = 0; i < size; i++)
+            {
+                if (array[i] < smallest)
+                {
+                    smallest = array[i];
+                    positionOfSmallest = i;
+                }
+            }
+            secondLargest = array[0];
+            for (i = 0; i < size; i++)
+            {
+                if (i == positionOfLargest)
+                {
+                    i++;
+                    i--;
+                }
+                else
+                {
+                    if (secondLargest < array[i])
+                    {
+                        secondLargest = array[i];
+                    }
+                }
+            }
+            secondSmallest = array[0];
+            for (i = 0; i < size; i++)
+            {
+                if (i == positionOfSmallest)
+                {
+                    i++;
+                    i--;
+                }
+                else
+                {
+                    if (secondSmallest > array[i])
+                    {
+                        secondSmallest = array[i];
+                    }
+                }
+            }
+
+            Console.WriteLine("The Second Largest element in the array is "+secondLargest);
+            Console.WriteLine("The Second Smallest element in the array is " + secondSmallest);
+
+        }
+    }
+}
